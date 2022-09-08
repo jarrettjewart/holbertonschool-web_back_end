153e9858ccdafb24ddf74567ff730b70d160cdfa
jarrettjewart <92384372+jarrettjewart@users.noreply.github.com>
2022-09-08 14:35:42 -0500

    vs code push

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,8 @@
+export default function appendToEachArrayValue(array, appendString) {
+    const tmp = [];
+    for (const idx of array) {
+      tmp.push(appendString + idx);
+    }
+  
+    return tmp;
+  }
